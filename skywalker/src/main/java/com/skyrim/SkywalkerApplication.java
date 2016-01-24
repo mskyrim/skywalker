fef01475a15d281e814e0dd78a3dd87f982cfa8b
Mehdi <dardani.mehdi@gmail.com>
2016-01-24 19:43:20 +0100

    skywalker project

---
+++ skywalker/src/main/java/com/skyrim/SkywalkerApplication.java
@@ -0,0 +1,12 @@
+package com.skyrim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkywalkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkywalkerApplication.class, args);
+	}
+}
