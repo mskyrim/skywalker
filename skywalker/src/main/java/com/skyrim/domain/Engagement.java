c05597cd6dc0f05574018e354029297645bb656c
Mehdi Dardani <dardani.mehdi@gmail.com>
2016-01-27 13:20:56 +0100

    DAO first step

---
+++ skywalker/src/main/java/com/skyrim/domain/Engagement.java
@@ -0,0 +1,64 @@
+package com.skyrim.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Engagement {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String titre;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dateCreation;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dateDebut;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dateFin;
+	private String description;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+	public Date getDateFin() {
+		return dateFin;
+	}
+	public void setDateFin(Date dateFin) {
+		this.dateFin = dateFin;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
---
+++ skywalker/src/main/java/com/skyrim/domain/Entreprise.java
@@ -0,0 +1,49 @@
+package com.skyrim.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Entreprise {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String nom;
+	private String typePrestation;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dateCreation;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getTypePrestation() {
+		return typePrestation;
+	}
+	public void setTypePrestation(String typePrestation) {
+		this.typePrestation = typePrestation;
+	}
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+	
+	
+}
